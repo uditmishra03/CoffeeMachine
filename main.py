9c32f423c3e1ef199c584458afcb0c17f8b20dd0
Udit <uditmishra.um@gmail.com>
2022-02-06 22:44:06 +0530

    Initial Rough draft of Todo and tasks to be performed.

---
+++ main.py
@@ -0,0 +1,28 @@
+# TODO: Prompt the user by giving choices of drinks.
+# take input from the user: “What would you like? (espresso/latte/cappuccino):”
+# There are hidden options too, report, this publishes the resources left at any given time
+# and "off" which terminates the machine/program for execution.
+
+# TODO: Print Report.
+# Report publishes the resources left at any given time.
+
+# TODO: Check resources sufficient?
+# When the user chooses a drink, the program should check if there are enough resources to make that drink.
+# water.”
+# The same should happen if another resource is depleted, e.g. milk or coffee.
+
+# TODO: Process Coins
+# a. If there are sufficient resources to make the drink selected, then the program should
+# prompt the user to insert coins.
+# b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
+
+# TODO: Check transaction successful?
+# Also calculate the value of instead coins and if more than the value of drink, offer change by subtracting the cost.
+# The cost of drink must be added to the Money earned by machine after each serving.
+
+# TODO: Make Coffee
+# a) If the transaction is successful and there are enough resources to make the drink the
+# user selected, then the ingredients to make the drink should be deducted from the
+# coffee machine resources
+# b) Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If
+# latte was their choice of drink.
